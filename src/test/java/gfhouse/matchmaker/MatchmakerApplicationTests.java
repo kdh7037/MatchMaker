6f5b9b33357edf8598b229af87ce6dd51219b191
kdh7037 <kdh7037@naver.com>
2022-12-29 03:10:39 +0900

    Init project

---
+++ src/test/java/gfhouse/matchmaker/MatchmakerApplicationTests.java
@@ -0,0 +1,13 @@
+package gfhouse.matchmaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatchmakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
