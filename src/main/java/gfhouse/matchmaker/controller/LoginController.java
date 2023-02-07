7d10a0138f87795882eee1474090becefd91dd2e
kdh7037 <kdh7037@naver.com>
2023-02-07 22:11:51 +0900

    Controller Modify

--- src/main/java/gfhouse/matchmaker/controller/LoginController.java
+++ src/main/java/gfhouse/matchmaker/controller/LoginController.java
@@ -4,18 +4,16 @@ import gfhouse.matchmaker.domain.User;
 import gfhouse.matchmaker.dto.LoginDto;
 import gfhouse.matchmaker.service.LoginService;
 import gfhouse.matchmaker.view.LoginView;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequiredArgsConstructor
 @RestController
 public class LoginController {
     private final LoginService loginService;
-
-    public LoginController(LoginService loginService) {
-        this.loginService = loginService;
-    }
 
     @PostMapping("/login")
     public LoginView login(@RequestBody LoginDto loginDto){
--- src/main/java/gfhouse/matchmaker/controller/UserController.java
+++ src/main/java/gfhouse/matchmaker/controller/UserController.java
@@ -4,6 +4,7 @@ import gfhouse.matchmaker.domain.User;
 import gfhouse.matchmaker.dto.UserDto;
 import gfhouse.matchmaker.service.UserService;
 import gfhouse.matchmaker.view.UserView;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,13 +12,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
+@RequiredArgsConstructor
 @RestController
 public class UserController {
     private final UserService userService;
-
-    public UserController(UserService userService) {
-        this.userService = userService;
-    }
 
     @PostMapping("/signup")
     public UserView signup(@RequestBody UserDto userDto){
