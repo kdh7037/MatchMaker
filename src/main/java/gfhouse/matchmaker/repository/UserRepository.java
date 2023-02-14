786eb4fd19332ee583a535b4faab8cef924a9297
kdh7037 <kdh7037@naver.com>
2023-02-14 23:27:56 +0900

    User signup and login modify

--- src/main/java/gfhouse/matchmaker/repository/UserRepository.java
+++ src/main/java/gfhouse/matchmaker/repository/UserRepository.java
@@ -7,6 +7,5 @@ import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    public Optional<User> findByUserId(String userId);
-    public Optional<User> findByPassword(String password);
+    Optional<User> findByUserId(String userId);
 }
