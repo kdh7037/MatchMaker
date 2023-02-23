cea4ee6d0f034d89423b9fbdfe9cedf5a949a2f3
carrot.ez(홍찬표)/kakao <carrot.ez@kakaocorp.com>
2023-02-23 10:12:29 +0900

    feat: swagger

--- src/main/java/gfhouse/matchmaker/controller/diary/DiaryController.java
+++ src/main/java/gfhouse/matchmaker/controller/diary/DiaryController.java
@@ -2,6 +2,8 @@ package gfhouse.matchmaker.controller.diary;
 
 import gfhouse.matchmaker.service.diary.DiaryService;
 import gfhouse.matchmaker.view.diary.DiaryView;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -13,32 +15,44 @@ import org.springframework.web.bind.annotation.*;
 public class DiaryController {
     private final DiaryService diaryService;
 
+    @Operation(summary = "플레이어 일기 작성")
     @PostMapping
     public String saveDiary(
-            @RequestParam String author,
-            @RequestParam String contents,
-            @RequestParam String title
+            @Parameter(description = "작성자") @RequestParam String author,
+            @Parameter(description = "제목") @RequestParam String title,
+            @Parameter(description = "본문") @RequestParam String contents
     ) {
         return diaryService.saveDiary(author, contents, title);
     }
 
+    @Operation(summary = "플레이어 일기 삭제")
     @DeleteMapping("/{diaryId}")
-    public String deleteDiary(@PathVariable Long diaryId) {
+    public String deleteDiary(@Parameter(description = "일기ID") @PathVariable Long diaryId) {
         return diaryService.deleteDiary(diaryId);
     }
 
+    @Operation(summary = "플레이어 일기 댓글 작성")
     @PostMapping("/{diaryId}/comments")
-    public String saveComment(@PathVariable Long diaryId, @RequestParam String author, @RequestParam String contents) {
+    public String saveComment(
+            @Parameter(description = "일기ID") @PathVariable Long diaryId,
+            @Parameter(description = "작성자") @RequestParam String author,
+            @Parameter(description = "댓글 내용") @RequestParam String contents
+    ) {
         return diaryService.saveComment(diaryId, author, contents);
     }
 
+    @Operation(summary = "플레이어 일기 댓글 삭제")
     @DeleteMapping("/{diaryId}/comments/{commentId}")
-    public String deleteComment(@PathVariable Long diaryId, @PathVariable Long commentId) {
+    public String deleteComment(
+            @Parameter(description = "일기ID") @PathVariable Long diaryId,
+            @Parameter(description = "댓글ID") @PathVariable Long commentId
+    ) {
         return diaryService.deleteComment(diaryId, commentId);
     }
 
+    @Operation(summary = "플레이어 일기 조회")
     @GetMapping("/{diaryId}")
-    public DiaryView getDiary(@PathVariable Long diaryId) {
+    public DiaryView getDiary(@Parameter(description = "일기ID") @PathVariable Long diaryId) {
         return diaryService.getDiaryView(diaryId);
     }
 }
--- src/main/java/gfhouse/matchmaker/view/diary/DiaryView.java
+++ src/main/java/gfhouse/matchmaker/view/diary/DiaryView.java
@@ -2,6 +2,7 @@ package gfhouse.matchmaker.view.diary;
 
 import gfhouse.matchmaker.domain.diary.Comment;
 import gfhouse.matchmaker.domain.diary.Diary;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,18 +12,34 @@ import java.time.LocalDateTime;
 import java.util.Collections;
 import java.util.List;
 
+@Schema(description = "일기 단건조회 응답")
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class DiaryView {
+    @Schema(description = "일기ID")
     private Long diaryId;
+
+    @Schema(description = "작성자")
     private String author;
+
+    @Schema(description = "제목")
     private String title;
+
+    @Schema(description = "본문")
     private String contents;
+
+    @Schema(description = "좋아요 수")
     private Long likes;
+
+    @Schema(description = "싫어요 수")
     private Long hates;
+
+    @Schema(description = "작성일자")
     private LocalDateTime createdAt;
+
+    @Schema(description = "댓글 목록")
     private List<CommentView> comments = Collections.emptyList();
 
     public static DiaryView of(Diary diary, List<Comment> comments) {
@@ -34,14 +51,22 @@ public class DiaryView {
     }
 }
 
+@Schema(description = "댓글 응답")
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 class CommentView {
+    @Schema(description = "댓글ID")
     private Long commentId;
+
+    @Schema(description = "댓글 작성자")
     private String author;
+
+    @Schema(description = "댓글 내용")
     private String contents;
+
+    @Schema(description = "작성일자")
     private LocalDateTime createdAt;
 
     public static CommentView of(Comment comment) {
